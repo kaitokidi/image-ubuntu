429f7a6970e9e5867fca4232bfe42db1195fbe47
Jérôme Tamba <jtamba@online.net>
2018-03-12 11:11:09 +0100

    Remove Zesty, add Artful from upstream

--- 17.10/env.mk
+++ 17.10/env.mk
@@ -1,9 +1,9 @@
 IMAGE_NAME = ubuntu
-IMAGE_VERSION = zesty
-IMAGE_VERSION_ALIASES = 17.04
-IMAGE_TITLE = Ubuntu Zesty
-IMAGE_DESCRIPTION = Ubuntu Zesty (17.04)
+IMAGE_VERSION = artful
+IMAGE_VERSION_ALIASES = 17.10
+IMAGE_TITLE = Ubuntu Artful Aardvark
+IMAGE_DESCRIPTION = Ubuntu Artful Aardvark (17.10)
 IMAGE_SOURCE_URL = https://github.com/scaleway/image-ubuntu
 IMAGE_VENDOR_URL = http://www.ubuntu.com
-IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BOOTSCRIPT = mainline 4.14
 IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
